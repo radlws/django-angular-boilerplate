bb3a3c27564d99b2b58f56546a34c16a98461cf3
radlws <radzhome@gmail.com>
2015-03-22 14:32:33 -0400

    added account serializer

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# Test account serializer
+
+# from authentication.models import Account
+# from authentication.serializers import AccountSerializer
+# account = Account.objects.latest('created_at')
+# serialized_account = AccountSerializer(account)
+# serialized_account.data.get('email')
+# serialized_account.data.get('username')
